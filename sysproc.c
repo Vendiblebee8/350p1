00c3ee13cfb555a24e2ae59dc650344ec74e2440
Ronald Ng <ronaldn1936@gmail.com>
2023-09-23 21:14:31 -0400

    implemented shutdown2

--- sysproc.c
+++ sysproc.c
@@ -80,6 +80,18 @@ sys_sleep(void)
 int sys_shutdown(void){
   outw(0xB004, 0x0|0x2000);
   outw(0x604, 0x0|0x2000);
+  return 0;
+}
+
+int sys_shutdown2(void) {
+  char* msg;
+  if(argstr(0, &msg) < 0) {
+    return -1;
+  }
+  cprintf("%s\n", msg);
+  outw(0xB004, 0x0|0x2000);
+  outw(0x604, 0x0|0x2000);
+  return 0;
 }
 
 // return how many clock tick interrupts have occurred
